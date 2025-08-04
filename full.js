a1cb1d481370a0a39bfea8e596cf7f16be14e928
parvez <parvezyesrat17032024@gmail.com>
2025-08-04 19:35:25 +0600

    added

--- full.js
+++ full.js
@@ -16,3 +16,15 @@ else if (money < 25000 && money >= 10000) {
 else {
     console.log('Chocolate');
 }
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 6 ;
+//write your code here
+for(var i=1;i<=lastDay;i++){
+    if(i%3==0){
+        console.log(i+' - medicine');
+    }
+    else{
+        console.log(i+' - rest');
+    }
+}
