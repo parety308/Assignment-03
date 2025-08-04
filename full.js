3d4e4210c25354394a2aadd85985207c16106539
parvez <parvezyesrat17032024@gmail.com>
2025-08-04 19:29:29 +0600

    added 2nd solution

--- full.js
+++ full.js
@@ -3,3 +3,16 @@ var area = 14.36666;
 //write your code here
  var results = area/2;
  console.log(results);
+
+ /** Problem -02 ( Cycle or Laptop ) */
+var money = 100;
+//write your code here
+if (money >= 25000) {
+    console.log('Laptop');
+}
+else if (money < 25000 && money >= 10000) {
+    console.log('Cycle');
+}
+else {
+    console.log('Chocolate');
+}
